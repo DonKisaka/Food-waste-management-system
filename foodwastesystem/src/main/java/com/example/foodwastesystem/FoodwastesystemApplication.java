5842317e00262d588e930d2992c1bec8fe0b2393
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-28 09:09:34 +0300

    first commit

---
+++ foodwastesystem/src/main/java/com/example/foodwastesystem/FoodwastesystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.foodwastesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodwastesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodwastesystemApplication.class, args);
+	}
+
+}
---
+++ foodwastesystem/src/test/java/com/example/foodwastesystem/FoodwastesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foodwastesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodwastesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
