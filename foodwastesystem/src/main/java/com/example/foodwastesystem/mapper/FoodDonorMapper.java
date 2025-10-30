79fd6b20ba2b43916e1030b0040b0dffaf56af55
DonaldKisaka <kisakadonald1@gmail.com>
2025-10-30 09:09:11 +0300

    FoodDonorMapper

--- foodwastesystem/src/main/java/com/example/foodwastesystem/mapper/FoodDonorMapper.java
+++ foodwastesystem/src/main/java/com/example/foodwastesystem/mapper/FoodDonorMapper.java
@@ -3,14 +3,13 @@ package com.example.foodwastesystem.mapper;
 import com.example.foodwastesystem.dto.FoodDonorRequestDto;
 import com.example.foodwastesystem.dto.FoodDonorResponseDto;
 import com.example.foodwastesystem.model.FoodDonor;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FoodDonorMapper {
+
     FoodDonorResponseDto toDto(FoodDonor foodDonor);
 
     @Mapping(target = "foodWasteItems", ignore = true)
@@ -20,5 +19,12 @@ public interface FoodDonorMapper {
     FoodDonor toEntity(FoodDonorRequestDto foodDonorRequestDto);
 
 
-    List<FoodDonorResponseDto> toDtos(List<FoodDonor> foodDonors);
+    List<FoodDonorResponseDto> toDto(List<FoodDonor> foodDonors);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "registrationDate", ignore = true)
+    @Mapping(target = "foodWasteItems", ignore = true)
+    @Mapping(target = "collectionCenters", ignore = true)
+    void updateFoodDonor(FoodDonorRequestDto dto, @MappingTarget FoodDonor foodDonor);
 }
