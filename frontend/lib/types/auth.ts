12bc779ae02c1677c036ffb223f0f89f42dd7d1e
DonaldKisaka <kisakadonald1@gmail.com>
2025-11-12 09:17:41 +0300

    Auth Response type

--- frontend/lib/types/auth.ts
+++ frontend/lib/types/auth.ts
@@ -1,4 +1,3 @@
-// Action Response Type for Server Actions
 export type ActionResponse =  {
   success: boolean;
   message: string;
@@ -6,25 +5,21 @@ export type ActionResponse =  {
   error?: string;
 };
 
-// Authentication Response from Spring Boot
 export interface AuthenticationResponse {
   token: string;
-  expiresIn: number;
+  email: string; 
 }
 
-// User Type
 export interface User {
   id: number;
   email: string;
 }
 
-// Signin Request
 export interface SignIn {
   email: string;
   password: string;
 }
 
-// Signup Request
 export interface Signup {
   email: string;
   password: string;
